a922f310ac7610ea8d9ddabb7813434ba566434a
dubiefe <emiliedubief@free.fr>
2025-11-28 08:51:35 +0100

    Initialize repo

---
+++ spotify-taste-mixer/src/app/contact/book/page.jsx
@@ -0,0 +1,10 @@
+export default function Book() {
+    return (
+        <div>
+            <h1>Reservation</h1>
+            <ul>
+                <li>Table</li>
+            </ul>
+        </div>
+    )
+}
